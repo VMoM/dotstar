436797db7b47ff95be8217774ded386137bd1cb1
VMoM <vmom.divers@gmail.com>
2021-05-08 21:52:42 +0200

    adaptation to the json

--- src/readers.py
+++ src/readers.py
@@ -1,5 +1,5 @@
 from typing import Dict, List
-import xml.etree.ElementTree as ElementTree
+import json
 
 from applications.application import Application
 
@@ -7,43 +7,35 @@ from applications.application import Application
 def read_application_store(filename: str) -> Dict[str, List[Application]]:
     """
     Extracts from the file asked the list of applications
-    :param filename: name of the file where the applications are stored
+    :param filename: name of the file where the applications are stored (json)
     :return: a dictionary of applications with the name of each category as a key and
              the list of applications in this category as a value
     """
-    # getting the parent tag
-    tree = ElementTree.parse(filename)
-    applications_xml = tree.getroot()
+    with open(filename, "r") as applications_file:
+        applications_data = json.load(applications_file)
 
-    # application_store is a dictionary of applications with
-    # - the name of each category as a key
-    # - the list of applications in this category as a value
-    application_store: Dict[str, List[Application]] = {}
+        # application_store is a dictionary of applications with
+        # - the name of each category as a key
+        # - the list of applications in this category as a value
+        application_store: Dict[str, List[Application]] = {}
 
-    # filling the application store from the XML
-    for application_xml in applications_xml:
-        # if the category is missing we add it
-        if application_xml.tag not in application_store.keys():
-            application_store[application_xml.tag] = []
+        # filling the application store from the JSON
+        for category_name in applications_data:
+            for application_data in applications_data[category_name]:
+                # if the category is missing we add it
+                if category_name not in application_store.keys():
+                    application_store[category_name] = []
 
-        # getting installation & deletion commands
-        installation_commands = {}
-        deletion_commands = {}
-        for pm in application_xml:
-            installation_commands[pm.tag] = pm.attrib["install"]
-            deletion_commands[pm.tag] = pm.attrib["delete"]
+                # adding a new Application in the list of the tag
+                application_store[category_name].append(
+                    Application(
+                        application_data["name"],
+                        application_data["description"],
+                        application_data["comment"],
+                        application_data["url"],
+                        application_data["paid"],
+                        application_data["PMs"]
+                    )
+                )
 
-        # adding a new Application in the list of the tag
-        application_store[application_xml.tag].append(
-            Application(
-                application_xml.attrib["name"],
-                application_xml.attrib["description"],
-                application_xml.attrib["comment"],
-                application_xml.attrib["url"],
-                application_xml.attrib["paid"] == "true",
-                installation_commands,
-                deletion_commands
-            )
-        )
-
-    return application_store
+        return application_store
